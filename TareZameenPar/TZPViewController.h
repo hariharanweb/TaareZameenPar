b54cb758b36e8d97f94db4baeca89961f0e6fe04
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-09-25 20:39:05 +0530

    V2, Hari | #00 | Added level 1 with few alphabets in place

--- TareZameenPar/TZPViewController.h
+++ TareZameenPar/TZPViewController.h
@@ -2,5 +2,13 @@
 
 @interface TZPViewController : UIViewController
 - (IBAction)playSound:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *alphabhet1;
+@property (weak, nonatomic) IBOutlet UIButton *alphabhet2;
+@property (weak, nonatomic) IBOutlet UIButton *alphabhet3;
+- (IBAction)incrementPage:(id)sender;
+- (IBAction)decrementPage:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *nextImage;
+@property (weak, nonatomic) IBOutlet UIButton *prevImage;
+
 
 @end
--- TareZameenPar/TZPViewController.m
+++ TareZameenPar/TZPViewController.m
@@ -11,22 +11,54 @@
 
 @interface TZPViewController ()
 @property AVAudioPlayer *avSound;
-
 @end
 
-@implementation TZPViewController
+@implementation TZPViewController {
+    char *selectedCharList;
+    int page;
+}
 @synthesize avSound;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    page = 0;
+    [self displayThreeAlphabets];
+    
+}
+
+-(void) displayThreeAlphabets {
+    char alphabets[7] = {'A','G','B','F','C','E','D'};
+    int startIndex = page * 3;
+    
+    UIImage *image1 = [UIImage imageNamed:[NSString stringWithFormat:@"%c.png",alphabets[startIndex]]];
+    UIImage *image2 = [UIImage imageNamed:[NSString stringWithFormat:@"%c.png",alphabets[startIndex+1]]];
+    UIImage *image3 = [UIImage imageNamed:[NSString stringWithFormat:@"%c.png",alphabets[startIndex+2]]];
+    
+    [[self alphabhet1] setBackgroundImage:image1 forState:UIControlStateNormal];
+    [[self alphabhet2] setBackgroundImage:image2 forState:UIControlStateNormal];
+    [[self alphabhet3] setBackgroundImage:image3 forState:UIControlStateNormal];
+    
+    [[self nextImage] setHidden:FALSE];
+    [[self prevImage] setHidden:FALSE];
+    
+    if(page == 2) {
+        [[self nextImage] setHidden:TRUE];
+    }
+    
+    if(page == 0){
+        [[self prevImage] setHidden:TRUE];
+    }
 }
 
 - (void)viewDidUnload
 {
+    [self setAlphabhet1:nil];
+    [self setAlphabhet2:nil];
+    [self setAlphabhet3:nil];
+    [self setNextImage:nil];
+    [self setPrevImage:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -34,10 +66,24 @@
     return YES;
 }
 
-- (IBAction)playSound:(id)sender {    
-    NSURL *soundURL = [[NSBundle mainBundle] URLForResource:@"a"
+- (IBAction)playSound:(id)sender {
+    char alphabets[7] = {'A','G','B','F','C','E','D'};
+    int startIndex = page * 3;
+    
+    int tag = ((UIButton*) sender).tag;
+    NSString *fileName = [NSString stringWithFormat:@"%c",(alphabets[startIndex+tag])];
+    NSLog(@"File name %@",fileName);
+    NSURL *soundURL = [[NSBundle mainBundle] URLForResource:fileName
                                               withExtension:@"mp3"];
     avSound = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];    
     [avSound play];
+}
+- (IBAction)incrementPage:(id)sender {
+    page++;
+    [self displayThreeAlphabets];
+}
+- (IBAction)decrementPage:(id)sender {
+    page--;
+    [self displayThreeAlphabets];
 }
 @end
